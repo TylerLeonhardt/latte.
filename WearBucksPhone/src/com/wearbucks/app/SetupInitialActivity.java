5f552251b7e09492629162832708505da4a785a7
tylerl0706 <tylerl0706@gmail.com>
2014-09-10 19:54:58 -0400

    New title, new font

--- WearBucksPhone/src/com/wearbucks/app/SetupInitialActivity.java
+++ WearBucksPhone/src/com/wearbucks/app/SetupInitialActivity.java
@@ -5,6 +5,7 @@ import java.text.NumberFormat;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,6 +13,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -33,6 +36,14 @@ public class SetupInitialActivity extends FragmentActivity implements RequestEve
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setup_initial);
         
+//        SpannableString s = new SpannableString("latte.");
+//        s.setSpan(new TypefaceSpan(this, "orator.ttf"), 0, s.length(),
+//                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Update the action bar title with the TypefaceSpan instance
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setTitle(s);
+        
         loginFragment = new LoginFragment();
         addCardFragment = new AddCardFragment();
         ft = getSupportFragmentManager().beginTransaction();
@@ -42,7 +53,8 @@ public class SetupInitialActivity extends FragmentActivity implements RequestEve
         
         if (savedInstanceState == null) {
             ft.replace(R.id.frame_location, new WelcomeFragment());
-            ft.commit();
+            ft.commit(); 
+           
         }
 	}
 	
--- WearBucksPhone/src/com/wearbucks/app/WelcomeFragment.java
+++ WearBucksPhone/src/com/wearbucks/app/WelcomeFragment.java
@@ -1,10 +1,12 @@
 package com.wearbucks.app;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class WelcomeFragment extends Fragment {
 	
@@ -12,6 +14,13 @@ public class WelcomeFragment extends Fragment {
 	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return view = inflater.inflate(R.layout.setup_welcome, container, false);
+    	view = inflater.inflate(R.layout.setup_welcome, container, false);
+    	
+    	TextView textview = (TextView) view.findViewById(R.id.welcome_text);
+    	Typeface font = Typeface.createFromAsset(inflater.getContext().getAssets(), "fonts/WeibeiSC.otf");
+    	textview.setTypeface(font);
+        
+    	
+    	return view;
     }
 }
