654c0241a0c6e626013fd74db9594253a1059bc3
Isabella Moreira <isabella@knights.ucf.edu>
2014-09-14 22:09:00 -0400

    updated package names

--- WearBucksPhone/src/com/wearbucks/app/RequestEventListener.java
+++
@@ -1,8 +0,0 @@
-package com.wearbucks.app;
-
-import org.json.JSONObject;
-
-public interface RequestEventListener {
-	public void onEventCompleted(JSONObject js);
-    public void onEventFailed();
-}
